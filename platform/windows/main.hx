120f06cb820a1aab44697b86e9af35c33a010f8f
AlvarroPewz <115935861+AlvarroPewz24@users.noreply.github.com>
2023-10-06 19:34:55 +0700

    Update main.hx

--- platform/windows/main.hx
+++ platform/windows/main.hx
@@ -5,12 +5,12 @@ import <game>.fps;
 
   run: "api/FPSLoaderAPI.hx"
 
-  #if win10+
+  #if win10, win11
   run: "api/FPSLoaderAPI.hx"
   run_command: delete C:\$main_game_directory\fps.xml
                create C:\$main_game_directory\fps.xml?source=[https://pewz-team.com/download/bin/fps.xml]
   #end
 
-  #if win8-
+  #if win8, win7, winVista
   show_message: "We do not support Windows 8-. We only supported Windows 10+"
   #end
