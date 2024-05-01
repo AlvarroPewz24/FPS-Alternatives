7c5dc9979554c05c48e2ed5e2d9e82d7bc98fec5
AlvarroPewz <115935861+AlvarroPewz24@users.noreply.github.com>
2024-05-01 11:58:35 +0700

    bro what da huel

--- api/FPSLoaderAPI.hx
+++
@@ -1,8 +0,0 @@
-package;
-
-class FPSLoader
-{
-	inline public static var API:String = "85075:WqA67t";
-	inline public static var EncKey:String = "6A9UD0DYW1";
-	inline public static var SESSION:String = null;
-}
